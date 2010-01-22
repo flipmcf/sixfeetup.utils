846f05a5ba5d29fdb47daa1a6651ea93b9a87617
Calvin Hendryx-Parker <calvinhp@sixfeetup.com>
2010-01-22 01:37:49 +0000

    bumping version after release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.2'
+version = '2.3dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
