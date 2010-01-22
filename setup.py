0ac9858db316a3c060d3c1d694951081e8a62ddd
Calvin Hendryx-Parker <calvinhp@sixfeetup.com>
2010-01-22 01:30:24 +0000

    Prepare sixfeetup.utils 2.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.1dev'
+version = '2.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
