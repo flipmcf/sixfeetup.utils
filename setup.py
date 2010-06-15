22ce69a41d886533524e37eb2023603a98e95cca
thomas <thomas@sixfeetup.com>
2010-06-15 15:58:58 +0000

    bumping versions after tagging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.4dev'
+version = '2.5dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
