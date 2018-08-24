c2f186ba42e0e4dc24e3f749792607b29e36127d
Chrissy Wainwright <chrissy@sixfeetup.com>
2018-08-24 14:24:29 -0400

    fix stupid typo, update version number

--- sixfeetup/utils/browser/monkeypatch.py
+++ sixfeetup/utils/browser/monkeypatch.py
@@ -5,7 +5,7 @@ logger = logging.getLogger("sixfeetup.utils")
 def notFoundError(self, entry='Unknown'):
     self.setStatus(404)
     if 'js' in entry or 'css' in entry:
-        logger.warn("{0} threw a 404, but we are bypassing the error for sanity's sake").format(entry)
+        logger.warn("{0} threw a 404, but we are bypassing the error for sanity's sake".format(entry))
 
 
 forbiddenError = notFoundError
